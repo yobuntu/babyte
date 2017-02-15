4c738dbcaa0d3783c1ed3319ab7a7610263a3814
Jordan Abderrhamane <jordan.abderrhamane@kozea.fr>
2017-02-15 18:03:06 +0100

    Les routes de l'application ont été écrites et il est maintenant possible d'ajouter un match et de le garder en mémoire

---
+++ babyte/__init__.py
@@ -0,0 +1,1 @@
+from .babyte import app
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='babyte',
+    packages=['babyte'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
