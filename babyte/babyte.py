033dc644cfebc40749fc14da91f5fca800c9f125
Jordan Abderrhamane <jordan.abderrhamane@kozea.fr>
2017-02-17 15:48:41 +0100

    list.html  has been updated to show a clear list of match with player names and scores

--- babyte/babyte.py
+++ babyte/babyte.py
@@ -14,6 +14,14 @@ app.config.update(dict(
 ))
 app.config.from_envvar('BABYTE_SETTINGS', silent=True)
 
+
+class User:
+    def __init__(self, name):
+        self.name = name
+        self.ranking = 1000
+        self.number_of_match = 0
+
+
 def connect_db():
     """Connects to the specific databse. """
     rv = sqlite3.connect(app.config['DATABASE'])
@@ -46,7 +54,8 @@ def initdb_command():
 
 @app.route('/')    
 def home():
-    return render_template('home.html')
+    ranking = compute_ranking()
+    return render_template('home.html', ranking=ranking)
 
 @app.route('/add', methods=['POST'])
 def add_match():
@@ -59,6 +68,104 @@ def add_match():
 @app.route('/list')    
 def list():
     db = get_db()
-    cur = db.execute('select id, team1_player1, team1_player2, team2_player1, team2_player2, score_team1, score_team2 from entries order by id desc')
+    cur = db.execute('select id, team1_player1, team1_player2, team2_player1, team2_player2, score_team1, score_team2 from match order by id desc')
     entries = cur.fetchall()
     return render_template('list.html', match=match)
+
+
+def compute_ranking():
+    """Get the list of all users with their current score."""
+    users = {name: User(name) for name in (
+        'Hugo', 'Jean-Pierre', 'Clément', 'Annabelle', 'Isabelle',
+        'Maxime')}
+
+    db = get_db()
+    cur = db.execute('select id, team1_player1, team1_player2, team2_player1, team2_player2, score_team1, score_team2 from match order by id asc')
+    matches = cur.fetchall()
+    for match in matches:
+        elo(users[match['team1_player1']], users[match['team1_player2']],
+            users[match['team2_player1']], users[match['team2_player2']],
+            match['score_team1'], match['score_team2'])
+    return users
+
+        
+def elo(team1_player1, team1_player2, team2_player1, team2_player2,
+            score_team1, score_team2):
+        """Update the ranking of each players in parameters.
+        Calculate the score of the match according to the following formula:
+        Rn = Ro + KG (W - We)
+        See: https://fr.wikipedia.org /wiki/Classement_mondial_de_football_Elo
+        """
+        elo1, number_match1 = fictive_player(team1_player1, team1_player2)
+        elo2, number_match2 = fictive_player(team2_player1, team2_player2)
+
+        score_p1 = compute_fictive_score(score_team1, score_team2,
+                                         elo1, elo2, number_match1)
+        score_p2 = compute_fictive_score(score_team2, score_team1,
+                                         elo2, elo1, number_match2)
+
+        update_score(team1_player1, team1_player2, score_p1)
+        update_score(team2_player1, team2_player2, score_p2)
+        
+
+def fictive_player(player_1, player_2):
+    """Create fictive player for elo computation."""
+    if player_2 is None:
+        elo = player_1.ranking
+        number_match = player_1.number_of_match
+    else:
+        elo = (player_1.ranking + player_2.ranking) / 2
+        number_match = (
+            player_1.number_of_match +
+            player_2.number_of_match) / 2
+    return elo, number_match
+   
+
+def compute_fictive_score(score_team, score_opponent, elo_team, elo_opponent,
+                          nb_match_team):
+    """Compute fictive score for elo ranking."""
+    expected_result = 1 / (1 + 10 ** ((elo_opponent - elo_team) / 400))
+    expertise = get_expertise_coefficient(nb_match_team, elo_team)
+    goal_difference = get_goal_difference_coefficient(
+        score_team, score_opponent)
+    result = 1 if score_team > score_opponent else 0
+    score = expertise * goal_difference * (result - expected_result)
+    print(expertise, goal_difference, score_team, score_opponent, result, expected_result, score)
+    return score
+    
+
+def get_expertise_coefficient(number_of_match, elo):
+    """Get expertise coefficient corresponding to an user's elo and matches."""
+    if number_of_match < 40:
+        return 40
+    elif elo < 2400:
+        return 20
+    else:
+        return 10
+        
+
+def get_goal_difference_coefficient(score_team_1, score_team_2):
+    """Get goal difference coefficient corresponding to a match score."""
+    diff = abs(score_team_1 - score_team_2)
+    if diff < 2:
+        return 1
+    elif diff == 2:
+        return 1.5
+    elif diff == 3:
+        return 1.75
+    else:
+        return 1.75 + (diff - 3) / 8
+    
+
+def update_score(player_1, player_2, score):
+    """Update score during elo computation."""
+    player_1.number_of_match += 1
+    if player_2 is None:
+        player_1.ranking += round(score)
+    else:
+        sum_ranking = player_1.ranking + player_2.ranking
+        player_1.ranking += round(
+            player_1.ranking / sum_ranking * score)
+        player_2.ranking += round(
+            player_2.ranking / sum_ranking * score)
+        player_2.number_of_match += 1
