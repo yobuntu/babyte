5b04d4d320af7e8b9e75428728cd24ca7a2ae50d
Hugo Périer <hugo.perier@kozea.fr>
2017-05-11 15:31:45 +0200

    Create style for  New-game section

--- babyte/babyte.py
+++ babyte/babyte.py
@@ -9,15 +9,27 @@ from flask import (
 from oauth2client.client import OAuth2WebServerFlow
 import httplib2
 import sqlite3
-        
+
 app = Flask(__name__) #create the application instance :)
 app.config.update(dict(
     DATABASE=os.path.join(app.root_path, 'babyte.db'),
     SECRET_KEY='development key',
+    OAUTH_CLIENT_ID='920868201743-f5p9lof7deojohp5n7t4rail5q4t80g8.apps.googleusercontent.com',
+    OAUTH_SECRET_KEY = 'lZf7gI5QYYWbaDOvUG_ARH6Q',
+    OAUTH_REDIRECT = 'http://localhost:5000/oauth2callback',
+    OAUTH_SCOPE = (
+        'https://www.googleapis.com/auth/contacts.readonly',
+        'https://www.googleapis.com/auth/plus.login'),
     USERNAME='admin',
     PASSWORD='default'
 ))
 app.config.from_envvar('BABYTE_SETTINGS', silent=True)
+
+
+if app.debug:
+    from sassutils.wsgi import SassMiddleware
+    app.wsgi_app = SassMiddleware(app.wsgi_app, {
+        'babyte': ('static', 'static', '/static')})
 
 
 class User:
@@ -39,7 +51,7 @@ def connect_db():
     rv = sqlite3.connect(app.config['DATABASE'])
     rv.row_factory = sqlite3.Row
     return rv
-  
+
 def get_db():
     """Opens a new database connection if there is none yet for the current application context."""
     if not hasattr(g, 'sqlite_db'):
@@ -54,7 +66,7 @@ def auth(function):
             return function(*args, **kwargs)
         return redirect(FLOW.step1_get_authorize_url())
     return wrapper
-    
+
 @app.route('/oauth2callback')
 def oauth2callback():
     code = request.args.get('code')
@@ -73,23 +85,22 @@ def oauth2callback():
         users_data = json.loads(users_content.decode('utf-8'))
         session['users'] = []
         for connection in users_data['connections']:
-            if 'names' in connection and 'emailAddresses' in connection:
+            if 'emailAddresses' in connection:
                 for address in connection['emailAddresses']:
                     if address['value'].endswith('@kozea.fr'):
-                        session['users'].append('%s %s' % (
-                            connection['names'][0].get('givenName', ''),
-                            connection['names'][0].get('familyName', '')))
+                        fullname = address['value'].split('@')[0]
+                        session['users'].append(' '.join(fullname.split('.')))
                         break
     else:
         return redirect(url_for('not_allowed'))
     return redirect(url_for('home'))
-        
+
 @app.teardown_appcontext
 def close_db(error):
     """Closes the database again at the end of the request."""
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
-    
+
 def init_db():
     db = get_db()
     with app.open_resource('babyte.sql', mode='r') as f:
@@ -103,7 +114,7 @@ def initdb_command():
     print('Initialized the database.')
 
 @app.route('/')
-@auth    
+@auth
 def home():
     ranking = compute_ranking()
     return render_template('home.html', ranking=ranking)
@@ -118,7 +129,7 @@ def add_match():
     return redirect(url_for('home'))
 
 @app.route('/list')
-@auth    
+@auth
 def list():
     db = get_db()
     cur = db.execute('select id, team1_player1, team1_player2, team2_player1, team2_player2, score_team1, score_team2 from match order by id desc')
@@ -139,25 +150,25 @@ def compute_ranking():
             match['score_team1'], match['score_team2'])
     return users
 
-        
+
 def elo(team1_player1, team1_player2, team2_player1, team2_player2,
             score_team1, score_team2):
-        """Update the ranking of each players in parameters.
-        Calculate the score of the match according to the following formula:
-        Rn = Ro + KG (W - We)
-        See: https://fr.wikipedia.org /wiki/Classement_mondial_de_football_Elo
-        """
-        elo1, number_match1 = fictive_player(team1_player1, team1_player2)
-        elo2, number_match2 = fictive_player(team2_player1, team2_player2)
+    """Update the ranking of each players in parameters.
+    Calculate the score of the match according to the following formula:
+    Rn = Ro + KG (W - We)
+    See: https://fr.wikipedia.org /wiki/Classement_mondial_de_football_Elo
+    """
+    elo1, number_match1 = fictive_player(team1_player1, team1_player2)
+    elo2, number_match2 = fictive_player(team2_player1, team2_player2)
 
-        score_p1 = compute_fictive_score(score_team1, score_team2,
-                                         elo1, elo2, number_match1)
-        score_p2 = compute_fictive_score(score_team2, score_team1,
-                                         elo2, elo1, number_match2)
+    score_p1 = compute_fictive_score(score_team1, score_team2,
+                                     elo1, elo2, number_match1)
+    score_p2 = compute_fictive_score(score_team2, score_team1,
+                                     elo2, elo1, number_match2)
 
-        update_score(team1_player1, team1_player2, score_p1)
-        update_score(team2_player1, team2_player2, score_p2)
-        
+    update_score(team1_player1, team1_player2, score_p1)
+    update_score(team2_player1, team2_player2, score_p2)
+
 
 def fictive_player(player_1, player_2):
     """Create fictive player for elo computation."""
@@ -170,7 +181,7 @@ def fictive_player(player_1, player_2):
             player_1.number_of_match +
             player_2.number_of_match) / 2
     return elo, number_match
-   
+
 
 def compute_fictive_score(score_team, score_opponent, elo_team, elo_opponent,
                           nb_match_team):
@@ -182,7 +193,7 @@ def compute_fictive_score(score_team, score_opponent, elo_team, elo_opponent,
     result = 1 if score_team > score_opponent else 0
     score = expertise * goal_difference * (result - expected_result)
     return score
-    
+
 
 def get_expertise_coefficient(number_of_match, elo):
     """Get expertise coefficient corresponding to an user's elo and matches."""
@@ -192,7 +203,7 @@ def get_expertise_coefficient(number_of_match, elo):
         return 20
     else:
         return 10
-        
+
 
 def get_goal_difference_coefficient(score_team_1, score_team_2):
     """Get goal difference coefficient corresponding to a match score."""
@@ -205,7 +216,7 @@ def get_goal_difference_coefficient(score_team_1, score_team_2):
         return 1.75
     else:
         return 1.75 + (diff - 3) / 8
-    
+
 
 def update_score(player_1, player_2, score):
     """Update score during elo computation."""
