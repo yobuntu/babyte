6534055ed8d52b240f994075e81d1030894b404c
grewn0uille <lucieanglade.gm@gmail.com>
2017-05-19 15:08:30 +0200

    Add Makefile

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
-from setuptools import setup
+from setuptools import find_packages, setup
+
+tests_requirements = [
+    'pytest',
+    'pytest-cov',
+    'pytest-flake8',
+    'pytest-isort',
+]
 
 setup(
     name='babyte',
-    packages=['babyte'],
+    author='Kozea',
+    packages=find_packages(),
     include_package_data=True,
+    scripts=['babyte/babyte.py'],
     install_requires=[
         'flask',
-	'oauth2client'
+	    'oauth2client'
     ],
+    tests_require=tests_requirements,
+    extras_require={'test':tests_requirements}
 )
